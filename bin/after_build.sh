6fad74c4974bc163dcc0e40aea74802bb08a70bb
Robin van der Vleuten <robinvdvleuten@gmail.com>
2012-06-20 15:15:27 +0200

    Basic files needed for PagodaBox deployment

---
+++ bin/after_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PHP=php
+
+echo 'Copy the distributed YAML parameters to the required parameters.yml.'
+cp app/config/parameters.dist.yml app/config/parameters.yml
+
+echo 'Download the composer.phar file, so the vendors can be installed from the distributed composer.json.'
+if [ ! -f composer.phar ]
+    then
+        curl -s -O http://getcomposer.org/composer.phar
+fi
+
+echo 'Install the needed vendors for this application.'
+$PHP composer.phar install --quiet
